6cb3717cbc430b888065ed9a3b384a5c12efb513
desarrolloti52 <desarrolloti52_lem@castores.com.mx>
2024-07-31 12:23:47 -0600

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { NgModule } from '@angular/core';
 
+import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    ListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CounterModule
+    CounterModule,
+    HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/counter/counter.module.ts
+++ src/app/counter/counter.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CounterComponent } from "./components/counter/counter.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
     declarations:[
         CounterComponent
     ],
-    imports:[
+    exports:[
         CounterComponent
+    ],
+    imports:[
+        CommonModule
     ],
 })
 
